52fd46fd9ad2ef580d529e6cf3481c459bc0d8f3
Rhino <Rhino@DEVICE-N740P235>
2024-12-06 15:26:15 +0600

    Add project files.

---
+++ WallpaperCopierApp/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace WallpaperCopierApp
+{
+    partial class WallpaperCopier
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(WallpaperCopier));
+            this.btnTheme = new System.Windows.Forms.Button();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnAbout = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.txtSaveLocation = new System.Windows.Forms.TextBox();
+            this.previewBox = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.lblStatus = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.previewBox)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnTheme
+            // 
+            this.btnTheme.Location = new System.Drawing.Point(569, 46);
+            this.btnTheme.Name = "btnTheme";
+            this.btnTheme.Size = new System.Drawing.Size(28, 23);
+            this.btnTheme.TabIndex = 0;
+            this.btnTheme.Text = "🌞";
+            this.btnTheme.UseVisualStyleBackColor = true;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(522, 131);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowse.TabIndex = 1;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(370, 208);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 2;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            // 
+            // btnAbout
+            // 
+            this.btnAbout.Location = new System.Drawing.Point(289, 208);
+            this.btnAbout.Name = "btnAbout";
+            this.btnAbout.Size = new System.Drawing.Size(75, 23);
+            this.btnAbout.TabIndex = 3;
+            this.btnAbout.Text = "About";
+            this.btnAbout.UseVisualStyleBackColor = true;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(479, 186);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(118, 45);
+            this.btnSave.TabIndex = 4;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // txtSaveLocation
+            // 
+            this.txtSaveLocation.Location = new System.Drawing.Point(289, 134);
+            this.txtSaveLocation.Name = "txtSaveLocation";
+            this.txtSaveLocation.Size = new System.Drawing.Size(218, 20);
+            this.txtSaveLocation.TabIndex = 5;
+            // 
+            // previewBox
+            // 
+            this.previewBox.Location = new System.Drawing.Point(12, 95);
+            this.previewBox.Name = "previewBox";
+            this.previewBox.Size = new System.Drawing.Size(255, 145);
+            this.previewBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.previewBox.TabIndex = 6;
+            this.previewBox.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.panel1.Controls.Add(this.titleLabel);
+            this.panel1.Controls.Add(this.btnTheme);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(609, 81);
+            this.panel1.TabIndex = 7;
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 36F, System.Drawing.FontStyle.Bold);
+            this.titleLabel.ForeColor = System.Drawing.Color.DarkGray;
+            this.titleLabel.Location = new System.Drawing.Point(3, 23);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(346, 58);
+            this.titleLabel.TabIndex = 1;
+            this.titleLabel.Text = "Wallpaper Copier";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Crimson;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(609, 4);
+            this.panel2.TabIndex = 8;
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(290, 186);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(74, 13);
+            this.lblStatus.TabIndex = 9;
+            this.lblStatus.Text = "Status: Ready";
+            // 
+            // WallpaperCopier
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(609, 261);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.previewBox);
+            this.Controls.Add(this.txtSaveLocation);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnAbout);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnBrowse);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "WallpaperCopier";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Wallpaper Copier";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            ((System.ComponentModel.ISupportInitialize)(this.previewBox)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTheme;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnAbout;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.TextBox txtSaveLocation;
+        private System.Windows.Forms.PictureBox previewBox;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label lblStatus;
+    }
+}
+
---
+++ WallpaperCopierApp/Form1.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using Newtonsoft.Json;
+
+
+namespace WallpaperCopierApp
+{
+    public partial class WallpaperCopier : Form
+
+    {
+        private bool dragging = false;
+        private Point dragCursorPoint;
+        private Point dragFormPoint;
+
+        public WallpaperCopier()
+        {
+            InitializeComponent();
+
+            // Load configuration
+            AppConfig config = LoadConfig();
+
+            // Set initial theme state
+            this.BackColor = config.IsLightTheme ? Color.White : Color.FromArgb(45, 45, 48);
+            btnTheme.Text = config.IsLightTheme ? "🌙" : "🌞"; // Set initial emoji based on theme
+            lblStatus.ForeColor = config.IsLightTheme ? Color.Black : Color.White; // Set initial status label color
+
+            // Event Handlers
+            this.Load -= new EventHandler(Form1_Load);
+            this.Load += new EventHandler(Form1_Load);
+
+            this.FormClosing -= new FormClosingEventHandler(Form1_FormClosing);
+            this.FormClosing += new FormClosingEventHandler(Form1_FormClosing);
+
+            btnBrowse.Click -= new EventHandler(BtnBrowse_Click);
+            btnBrowse.Click += new EventHandler(BtnBrowse_Click);
+
+            btnTheme.Click -= new EventHandler(BtnTheme_Click);
+            btnTheme.Click += new EventHandler(BtnTheme_Click);
+
+            btnAbout.Click -= new EventHandler(BtnAbout_Click);
+            btnAbout.Click += new EventHandler(BtnAbout_Click);
+
+            btnSave.Click -= new EventHandler(BtnSave_Click);
+            btnSave.Click += new EventHandler(BtnSave_Click);
+
+            btnExit.Click -= new EventHandler(BtnExit_Click);
+            btnExit.Click += new EventHandler(BtnExit_Click);
+
+            this.FormBorderStyle = FormBorderStyle.None; // Remove title bar
+
+            this.MouseDown -= new MouseEventHandler(Form1_MouseDown);
+            this.MouseDown += new MouseEventHandler(Form1_MouseDown);
+
+            this.MouseMove -= new MouseEventHandler(Form1_MouseMove);
+            this.MouseMove += new MouseEventHandler(Form1_MouseMove);
+
+            this.MouseUp -= new MouseEventHandler(Form1_MouseUp);
+            this.MouseUp += new MouseEventHandler(Form1_MouseUp);
+
+            // Add mouse event handlers for all controls to enable dragging
+            foreach (Control control in this.Controls)
+            {
+                control.MouseDown += new MouseEventHandler(Form1_MouseDown);
+                control.MouseMove += new MouseEventHandler(Form1_MouseMove);
+                control.MouseUp += new MouseEventHandler(Form1_MouseUp);
+            }
+
+            // Add mouse event handlers for panel1 to enable dragging
+            panel1.MouseDown += new MouseEventHandler(Form1_MouseDown);
+            panel1.MouseMove += new MouseEventHandler(Form1_MouseMove);
+            panel1.MouseUp += new MouseEventHandler(Form1_MouseUp);
+
+            // Add mouse event handlers for panel2 to enable dragging
+            panel2.MouseDown += new MouseEventHandler(Form1_MouseDown);
+            panel2.MouseMove += new MouseEventHandler(Form1_MouseMove);
+            panel2.MouseUp += new MouseEventHandler(Form1_MouseUp);
+        }
+        private AppConfig LoadConfig()
+        {
+            string configPath = Path.Combine(Path.GetTempPath(), "WC-Config");
+            if (File.Exists(configPath))
+            {
+                string json = File.ReadAllText(configPath);
+                return JsonConvert.DeserializeObject<AppConfig>(json) ?? new AppConfig();
+            }
+            return new AppConfig();
+        }
+
+        private void SaveConfig(AppConfig config)
+        {
+            string configPath = Path.Combine(Path.GetTempPath(), "WC-Config");
+            string json = Newtonsoft.Json.JsonConvert.SerializeObject(config, Newtonsoft.Json.Formatting.Indented);
+            File.WriteAllText(configPath, json);
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtSaveLocation.Text = Properties.Settings.Default.LastUsedDirectory;
+            LoadWallpaperPreview();
+            btnTheme.Text = "🌞"; // Initialize with sun emoji
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Properties.Settings.Default.LastUsedDirectory = txtSaveLocation.Text;
+            Properties.Settings.Default.Save();
+
+            // Save current theme state to config
+            AppConfig config = new AppConfig { IsLightTheme = (this.BackColor == Color.White) };
+            SaveConfig(config);
+        }
+        private void BtnBrowse_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog fbd = new FolderBrowserDialog())
+            {
+                if (fbd.ShowDialog() == DialogResult.OK)
+                {
+                    txtSaveLocation.Text = fbd.SelectedPath;
+                }
+            }
+        }
+        private void BtnTheme_Click(object sender, EventArgs e)
+        {
+            bool isLightTheme = this.BackColor == Color.White;
+            this.BackColor = isLightTheme ? Color.FromArgb(45, 45, 48) : Color.White; // #2d2d30 is equivalent to (45, 45, 48) in RGB
+            btnTheme.Text = isLightTheme ? "🌙" : "🌞"; // Switch between sun and moon emojis
+
+            // Update status label color based on theme
+            lblStatus.ForeColor = isLightTheme ? Color.White : Color.Black;
+            lblStatus.Text = "Theme switched to " + (isLightTheme ? "dark" : "light") + " mode.";
+
+            // Save theme state to config
+            AppConfig config = new AppConfig { IsLightTheme = !isLightTheme };
+            SaveConfig(config);
+        }
+        private void BtnAbout_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Wallpaper Copier\nVersion 2.0\nCreated by Imran Ahmed");
+        }
+        private void BtnSave_Click(object sender, EventArgs e)
+        {
+            string wallpaperPath = GetCurrentWallpaper();
+            if (!string.IsNullOrEmpty(wallpaperPath) && File.Exists(wallpaperPath))
+            {
+                SaveWallpaper(wallpaperPath);
+                lblStatus.Text = "Wallpaper saved successfully!";
+            }
+            else
+            {
+                lblStatus.Text = "Failed to save wallpaper. Could not find the current wallpaper.";
+            }
+        }
+        private void BtnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            dragging = true;
+            dragCursorPoint = Cursor.Position;
+            dragFormPoint = this.Location;
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (dragging)
+            {
+                Point diff = Point.Subtract(Cursor.Position, new Size(dragCursorPoint));
+                this.Location = Point.Add(dragFormPoint, new Size(diff));
+            }
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            dragging = false;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private string GetCurrentWallpaper()
+        {
+            string wallpaperPath = Path.Combine(
+                Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
+                "Microsoft\\Windows\\Themes\\TranscodedWallpaper"
+            );
+            return wallpaperPath;
+        }
+        private void SaveWallpaper(string sourcePath)
+        {
+            string saveLocation = txtSaveLocation.Text;
+            string fileName = Path.GetFileNameWithoutExtension(sourcePath);
+            string date = DateTime.Now.ToString("MMddyyyy");
+            string part = "01";
+            string newFileName;
+            int partNumber = 1;
+
+            do
+            {
+                newFileName = $"{fileName}_01{date}{part.PadLeft(2, '0')}.jpg";
+                partNumber++;
+                part = partNumber.ToString();
+            } while (File.Exists(Path.Combine(saveLocation, newFileName)));
+
+            string destFile = Path.Combine(saveLocation, newFileName);
+            File.Copy(sourcePath, destFile);
+        }
+        private void LoadWallpaperPreview()
+        {
+            string wallpaperPath = GetCurrentWallpaper();
+            if (File.Exists(wallpaperPath))
+            {
+                previewBox.Image = Image.FromFile(wallpaperPath);
+            }
+            else
+            {
+                MessageBox.Show("Could not find the current wallpaper.");
+            }
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}    public class AppConfig
+    {
+        public bool IsLightTheme { get; set; } = true;
+    }
